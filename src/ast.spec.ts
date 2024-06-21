d246ae82f5abbf31e80386c89dfa96bf154e7f8a
justL <justlikesuolong@outlook.com>
2024-06-21 11:42:26 +0800

    fix: 大意失荆州

--- src/ast.spec.ts
+++ src/ast.spec.ts
@@ -64,7 +64,7 @@ function ReactComponent() {
        * 读取 ./codetxt/sign-in.txt 文件内容
        */
       const sourceCode = fs.readFileSync(path.resolve('src/codetxt/sign-in.txt')).toString()
-      const result = (await pluginPack() as any)?.transform(sourceCode, 'src/codetxt/sign-in.ts')
+      const result = await (pluginPack() as any)?.transform(sourceCode, 'src/codetxt/sign-in.ts')
       expect(result).contain('export async function signIn')
       expect(result).contain('-sign-in-signIn\'')
     },
